8798bb66417fda84159f19bdc29735063fc93da9
Raiu1210 <baio1484@gmail.com>
2022-07-30 14:13:44 +0900

    add some view

--- pages/search.vue
+++ pages/search.vue
@@ -1,20 +1,111 @@
 <template>
-  <h1>Search</h1>
+  <div>
+    <v-card>
+      <GmapMap
+        ref="gmp"
+        map-type-id="roadmap"
+        :center="maplocation"
+        :zoom="zoom"
+        :style="styleMap"
+        :options="mapOptions"
+      >
+        <GmapMarker
+          v-for="(onsen, index) in onsenList"
+          :key="index"
+          :title="onsen.name"
+          :position="{lat: onsen.lat, lng: onsen.lon}"
+          :clickable="true"
+          :draggable="false"
+          @click="onClickMarker(index, onsen)"
+        />
+        <GmapInfoWindow
+          :options="infoOptions"
+          :position="infoWindowPos"
+          :opened="infoWinOpen"
+          @closeclick="infoWinOpen = false"
+        >
+          <center style="color: #000">
+            {{ marker.name }}
+          </center>
+        </GmapInfoWindow>
+      </GmapMap>
+    </v-card>
+
+    <v-card class="mx-auto my-4">
+      <v-img
+        height="150"
+        :src="'http://localhost:8000/thumbnails/' + marker['image_path']"
+      ></v-img>
+
+      <v-card-title>{{marker['name']}}</v-card-title>
+      <v-card-text class="text--primary">
+        {{marker['address']}} <br />
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import {axiosInstance as Api} from '~/myModules/api'
+import {createInstanceWithJWT} from '~/myModules/api'
 import $cookies from "cookie-universal-nuxt";
 
 
 export default {
   data() {
     return {
-      cards: [
-        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
-        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
-        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
-      ],
+      onsenList: [],
+      maplocation: { lat: 0, lng: 0 },
+      zoom: 12,
+      styleMap: {
+        width: '100%',
+        height: '400px',
+      },
+      mapOptions: {
+        streetViewControl: false,
+        styles: [],
+      },
+      infoOptions: {
+        minWidth: 200,
+        pixelOffset: {
+          width: 0,
+          height: -35,
+        },
+      },
+      infoWindowPos: null,
+      infoWinOpen: false,
+      marker: {}
     }
+  },
+  async created() {
+    if(typeof this.$cookies.get("jwt-token") === "undefined") {
+      console.log("cookie is empty. go to login")
+      this.$router.push('/login')
+    }
+  },
+  async mounted() {
+    const res = await Api.get('onsen/onsen_list')
+    this.onsenList = res['data']
+
+    const currentPosTmp = await this.getCurrentPosition()
+    const currentPos = {
+      lat: currentPosTmp.coords.latitude,
+      lng: currentPosTmp.coords.longitude,
+    }
+    this.maplocation = currentPos
+  },
+  methods: {
+    getCurrentPosition() {
+      return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+      })
+    },
+    onClickMarker(index, marker) {
+      this.$refs.gmp.panTo({lat: marker.lat, lng: marker.lon})
+      this.infoWindowPos = {lat: marker.lat, lng: marker.lon}
+      this.marker = marker
+      this.infoWinOpen = true
+    },
   },
   created() {
     // console.log(this.$cookies.get("jwt-token"))
--- pages/stats.vue
+++ pages/stats.vue
@@ -1,27 +1,148 @@
 <template>
-  <h1>Stats</h1>
+  <div>
+    <v-row
+      justify="center" align-content="center"
+    >
+      <v-col cols=12>
+        <v-card class="my-2">
+          <v-toolbar class="white--text" color="deep-orange darken-1" flat>訪問回数</v-toolbar>
+
+          <BarGraph class="mt-3" :chartData="chartData" />
+
+          <center>
+            <v-col cols=8>
+              <v-select
+                class="mt-3"
+                :items="visitedYears"
+                v-model="selectedYear"
+                :label="selectedYear + '年'"
+                solo
+              ></v-select>
+            </v-col>
+          </center>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row
+      justify="center" align-content="center"
+    >
+      <v-col cols=12>
+        <v-card class="my-2" >
+          <v-toolbar class="white--text" color="purple darken-1" flat>コンプ率</v-toolbar>
+
+          <DoughnutGraph class="mt-3" :dataLength="onsenList.length" :numVisited="visited.length" />
+          <center>
+            <v-col cols=8>
+              <v-select
+                class="mt-3"
+                :items="visitedYears"
+                v-model="selectedYear"
+                label="全国"
+                solo
+              ></v-select>
+            </v-col>
+          </center>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <center>
+      <v-btn
+        depressed
+        color="primary"
+        class="mx-2 my-2"
+        @click="$router.push('/history')"
+      >
+        チェックインの履歴
+      </v-btn>
+    </center>
+  </div>
 </template>
 
 <script>
+import {axiosInstance as Api} from '~/myModules/api'
+import {createInstanceWithJWT} from '~/myModules/api'
 import $cookies from "cookie-universal-nuxt";
 
+// my component
+import BarGraph from '~/components/BarGraph'
+import DoughnutGraph from '~/components/DoughnutGraph'
 
 export default {
   data() {
     return {
-      cards: [
-        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
-        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
-        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
-      ],
+      onsenList: [],
+      visited: [],
+      visitedYears: [],
+      selectedYear: 0,
+
+      // stats area
+      area: ['全国'],
+
+      // input for monthly bar graph
+      chartData: {}
+
     }
   },
-  created() {
-    // console.log(this.$cookies.get("jwt-token"))
-    // if(typeof this.$cookies.get("jwt-token") === "undefined") {
-    //   console.log("cookie is empty. go to login")
-    //   this.$router.push('/login')
-    // }
+  async created() {
+    console.log(this.$cookies.get("jwt-token"))
+    if(typeof this.$cookies.get("jwt-token") === "undefined") {
+      this.$router.push('/login')
+    }
+    const res = await Api.get('onsen/onsen_list')
+    this.onsenList = res['data']
+
+    const apiInstance = createInstanceWithJWT(this.$cookies.get("jwt-token"))
+    const res2 = await apiInstance.get('users/my_visit')
+    console.log(res2['data'])
+    this.visited = res2['data']
+
+    // get this year
+    const timeObj = new Date()
+    this.selectedYear = timeObj.getFullYear()
+    this.calcMonthlyCount(timeObj.getFullYear())
+    this.countVisitedYears()
+  },
+  methods: {
+    getTimeInfo(timeStr) {
+      const dateInfo = timeStr.split('T')[0].split('-')
+      return dateInfo[0] + '年' + dateInfo[1] + '月' + dateInfo[2] + '日'
+    },
+    calcMonthlyCount(year) {
+      let monthlyCount = {'1月':0,'2月':0,'3月':0,'4月':0,'5月':0,'6月':0,'7月':0,'8月':0,'9月':0,'10月':0,'11月':0,'12月':0}
+      this.visited.forEach(function(visit) {
+        const dateInfo = visit['created_at'].split('T')[0].split('-')
+        if(dateInfo[0] != year) {
+          return
+        }
+        monthlyCount[Number(dateInfo[1])+'月'] += 1
+      })
+      let chartData = {
+        labels: Object.keys(monthlyCount),
+        datasets: [{
+          label: "温泉にチェックインした回数",
+          backgroundColor: '#ff6600',
+          data: Object.values(monthlyCount)
+        }]
+      }
+      this.chartData = chartData
+    },
+    countVisitedYears() {
+      let years = []
+      this.visited.forEach(function(visit) {
+        const dateInfo = visit['created_at'].split('T')[0].split('-')
+        if(years.includes(Number(dateInfo[0])+'年')) {
+          return
+        }
+        years.push(Number(dateInfo[0])+'年')
+      })
+      this.visitedYears = years
+    }
+  },
+  components: {
+    BarGraph,
+    DoughnutGraph
   }
 }
 </script>
